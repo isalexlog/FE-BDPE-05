0e0827ed384876ad662fa0610355d8490f6e3093
isalexlog <is.alex.loginov@gmail.com>
2019-06-20 20:33:54 +0200

    Adding swagger to Java project

--- Employee/src/main/java/de/telran/employee/EmployeeApplication.java
+++ Employee/src/main/java/de/telran/employee/EmployeeApplication.java
@@ -2,11 +2,20 @@ package de.telran.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableSwagger2
+@Configuration
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
@@ -14,6 +23,12 @@ public class EmployeeApplication {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
-
-
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
